8ed128203060b1c1e276d9271125e601cbb6600a
Diaa Kasem <me@diaa.me>
2014-06-14 10:12:55 +0200

    Initial structure using yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularjsReportingApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
